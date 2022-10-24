c26664874fc47c9311acf62844fc8fe48713f4c2
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-10-24 22:43:11 +0100

    added the increment and decrement btns

--- src/App.js
+++ src/App.js
@@ -3,10 +3,14 @@ import { useSelector } from "react-redux";
 
 function App() {
   const counter = useSelector((state) => state.counter);
+
   return (
     <>
       <h2>Counter | Redux</h2>
       <h3>{counter}</h3>
+
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
     </>
   );
 }
