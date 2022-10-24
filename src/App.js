f26793502d5d8351eb10564bfc3bf50d7088e568
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-10-24 22:37:30 +0100

    chore: set the counter to zero

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
+import { useSelector } from "react-redux";
 
 function App() {
+  const counter = useSelector((state) => state.counter);
   return (
     <>
       <h2>Counter | Redux</h2>
+      <h3>{counter}</h3>
     </>
   );
 }
